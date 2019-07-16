426bfbc56fe829012a6e7ea50fa28145416d1610
LeeSangHa <38284431+StrangeLee@users.noreply.github.com>
2019-07-17 07:32:08 +0900

    shit6

--- app/src/main/java/com/example/a2019hack/Page/activity/LoginActivity.java
+++ app/src/main/java/com/example/a2019hack/Page/activity/LoginActivity.java
@@ -17,7 +17,9 @@ import com.example.a2019hack.data.Child;
 public class LoginActivity extends AppCompatActivity {
 
     String loginId, loginPassword;
-    String registerId, registerPassword;
+
+    final String userId = "user1234";
+    final String userPw = "1234";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,37 +30,30 @@ public class LoginActivity extends AppCompatActivity {
         EditText passwordText = (EditText) findViewById(R.id.loginPasswordText); //PW입력창
         Button loginBtn = (Button) findViewById(R.id.buttonLogin); // 로그인 버튼
         TextView registerText = (TextView) findViewById(R.id.loginGetRegister); // 회원가입 텍스트박스
-        CheckBox keepLoginBox = (CheckBox) findViewById(R.id.keepLoginCheckBox); // 로그인유지 체크박스
-
-        loginId = idText.getText().toString();
-        loginPassword = passwordText.getText().toString();
 
         //로그인 버튼 클릭리스너
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //registerId =
-                //registerPassword =
+        loginBtn.setOnClickListener(v -> {
 
-                if (loginId.equals(registerId) && loginPassword.equals(registerPassword)){
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                    startActivity(intent);
-                    Toast toast = Toast.makeText(getApplicationContext(), "로그인하였습니다.", Toast.LENGTH_LONG);
-                    toast.show();
-                } else {
-                    Toast toast = Toast.makeText(getApplicationContext(), "아이디 또는 비밀번호가 일치하지 않습니다.", Toast.LENGTH_LONG);
-                    toast.show();
-                }
+            loginId = idText.getText().toString();
+            loginPassword = passwordText.getText().toString();
+
+            if (loginId.equals(userId) && loginPassword.equals(userPw)){
+
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                Toast toast = Toast.makeText(getApplicationContext(), "로그인하였습니다.", Toast.LENGTH_LONG);
+                toast.show();
+            } else {
+                Toast toast = Toast.makeText(getApplicationContext(), "아이디 또는 비밀번호가 일치하지 않습니다.", Toast.LENGTH_LONG);
+                toast.show();
             }
         });
 
         //회원가입 텍스트 클릭 리스너
-        registerText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
-                startActivity(intent);
-            }
+        registerText.setOnClickListener(v -> {
+
+            Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+            startActivity(intent);
         });
 
     }
