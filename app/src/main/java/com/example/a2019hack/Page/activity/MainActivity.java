aad719b3b1c7e93e511814b57106ed1d8729d2e5
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-07-16 23:06:51 +0900

    .

--- app/src/main/java/com/example/a2019hack/Page/activity/MainActivity.java
+++ app/src/main/java/com/example/a2019hack/Page/activity/MainActivity.java
@@ -1,14 +1,11 @@
 package com.example.a2019hack.Page.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
