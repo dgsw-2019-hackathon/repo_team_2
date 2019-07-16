6bd296215b7806a696ad10e56c435ab4d365c1d2
LeeSangHa <38284431+StrangeLee@users.noreply.github.com>
2019-07-16 15:16:45 +0900

    hjjmj

--- app/src/main/java/com/example/a2019hack/Page/activity/AddChildActivity.java
+++ app/src/main/java/com/example/a2019hack/Page/activity/AddChildActivity.java
@@ -2,6 +2,9 @@ package com.example.a2019hack.Page.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 import com.example.a2019hack.R;
 
@@ -11,5 +14,15 @@ public class AddChildActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_child);
+
+        Spinner ageSpinner = findViewById(R.id.ageSpinner);
+
+        ageSpinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+
     }
 }
